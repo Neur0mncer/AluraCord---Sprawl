6d70d5b1e81ac00b1c2690a9d7b9c2f2eec59660
Pedro Mendes <perseukane@gmail.com>
2022-01-26 16:03:10 -0300

    FSociatyChat

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,24 @@
 import appConfig from '../config.json'
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
 import { urlObjectKeys } from 'next/dist/shared/lib/utils'
-
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        @import url('https://fonts.googleapis.com/css2?family=Train+One&display=swap'); 
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Train one', 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
+import React from 'react';
+import { useRouter } from 'next/router';
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
     return (
-      <>
-        <Tag>{props.children}</Tag>
-        <style jsx>{`
+        <>
+            <Tag>{props.children}</Tag>
+            <style jsx>{`
               ${Tag} {
                   color: ${appConfig.theme.colors.primary['800']};
                   font-size: 36px;
                   font-weight: 600;
               }
               `}</style>
-      </>
+        </>
     );
-  }
+}
 
 //Componente React
 // function HomePage() {
@@ -64,11 +35,11 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'Neur0mncer';
+    const [username, setUsername] = React.useState('Neur0mncer');
+    const roteamento = useRouter()
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -95,6 +66,12 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento) {
+                            infosDoEvento.preventDefault();
+                            console.log('Alguém submeteu o form');
+                            roteamento.push('/chat');
+                            // jeito tradicional (window.location.href = '/chat';)
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -106,6 +83,15 @@ export default function PaginaInicial() {
                         </Text>
 
                         <TextField
+                            value={username}
+                            onChange={function (event) {
+                                console.log('usuario digitou', event.target.value);
+                                // Onde ta o valor?
+                                const valor = event.target.value;
+                                // Trocar o valor da variavel
+                                // através do React e avise quem precisa
+                                setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
